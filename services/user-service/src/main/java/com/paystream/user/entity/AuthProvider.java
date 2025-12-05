e205adaba139145c9534a687b2850ad750a28359
mminzy22 <aw7022@gmail.com>
2025-12-05 14:41:36 +0900

    ✨ feat(user-service): AuthProvider enum 생성

---
+++ services/user-service/src/main/java/com/paystream/user/entity/AuthProvider.java
@@ -0,0 +1,9 @@
+package com.paystream.user.entity;
+
+public enum AuthProvider {
+    LOCAL, // 일반 회원가입
+    GOOGLE, // 구글 로그인 (나중에 추가)
+    KAKAO, // 카카오 로그인 (나중에 추가)
+    NAVER, // 네이버 로그인 (나중에 추가)
+    APPLE // 애플 로그인 (나중에 추가)
+}
