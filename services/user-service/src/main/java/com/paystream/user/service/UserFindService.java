70ac249dde1efb43449755aeda6270af17cedb49
mminzy22 <aw7022@gmail.com>
2025-12-05 16:06:03 +0900

    ✨ feat(user-service): UserFindService 구현

---
+++ services/user-service/src/main/java/com/paystream/user/service/UserFindService.java
@@ -0,0 +1,36 @@
+package com.paystream.user.service;
+
+import com.paystream.core.exception.ExceptionEnum;
+import com.paystream.core.exception.PayStreamException;
+import com.paystream.user.dto.response.UserResponse;
+import com.paystream.user.entity.User;
+import com.paystream.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class UserFindService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse findById(Long id) {
+        User user =
+                userRepository
+                        .findById(id)
+                        .orElseThrow(() -> new PayStreamException(ExceptionEnum.USER_NOT_FOUND));
+
+        return UserResponse.of(user);
+    }
+
+    public UserResponse findByEmail(String email) {
+        User user =
+                userRepository
+                        .findByEmail(email)
+                        .orElseThrow(() -> new PayStreamException(ExceptionEnum.USER_NOT_FOUND));
+
+        return UserResponse.of(user);
+    }
+}
