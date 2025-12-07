3fc5c45d7f594166273006095532cc9c11dbafcd
mminzy22 <aw7022@gmail.com>
2025-12-07 22:52:56 +0900

    🧪 test(user-service): 애플리케이션 컨텍스트 로드 테스트 추가

---
+++ services/user-service/src/test/java/com/paystream/user/UserServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.paystream.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(
+        properties = {
+            "eureka.client.enabled=false",
+            "eureka.client.register-with-eureka=false",
+            "eureka.client.fetch-registry=false"
+        })
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인
+    }
+}
