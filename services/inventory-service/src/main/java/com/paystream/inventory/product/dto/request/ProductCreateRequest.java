6c9b18a4edde01ee16ae4c0ffb480dc0521c341a
seonoo <tjsdn22@naver.com>
2025-12-15 21:32:08 +0900

    ♻ refactor(inventory-service): ProductCreateRequest의 validation 어노테이션 추가

--- services/inventory-service/src/main/java/com/paystream/inventory/product/dto/request/ProductCreateRequest.java
+++ services/inventory-service/src/main/java/com/paystream/inventory/product/dto/request/ProductCreateRequest.java
@@ -1,6 +1,7 @@
 package com.paystream.inventory.product.dto.request;
 
 import com.paystream.inventory.product.entity.Product;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,12 +21,22 @@ public class ProductCreateRequest {
     @NotBlank(message = "이름은 필수 입력입니다.")
     private String name;
 
+    @NotBlank(message = "설명은 반드시 입력해야합니다.")
     private String description;
 
-    private int minCapacity;
-    private int maxCapacity;
+    @Min(value = 1000, message = "기본 가격은 1000원 이상부터 입니다.")
     private int basePrice;
+
+    @Min(value = 1000, message = "추가금액은 1000원 이상부터 입니다.")
     private int personAddPrice;
+
+    @Min(value = 2, message = "최소인원은 2명 이상이어야 합니다.")
+    private int minCapacity;
+
+    @Min(value = 3, message = "최대인원은 3명 이상이어야 합니다.")
+    private int maxCapacity;
+
+    @Min(value = 1, message = "재고는 최소 1개 이상이어야 합니다.")
     private int stock; // 초기 재고 생산용
 
     //    private List<String> photos; // URL 목록
