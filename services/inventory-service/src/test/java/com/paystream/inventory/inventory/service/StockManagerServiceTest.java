3f1535d2a637e6b7506486b030723329325a4ce5
seonoo <tjsdn22@naver.com>
2026-03-09 22:25:32 +0900

    🐛 fix: 테스트 문제 수정
    - 추가 락을 위한 Thread.sleep 추가

--- services/inventory-service/src/test/java/com/paystream/inventory/inventory/service/StockManagerServiceTest.java
+++ services/inventory-service/src/test/java/com/paystream/inventory/inventory/service/StockManagerServiceTest.java
@@ -190,7 +190,7 @@ class StockManagerServiceTest {
 
     @DisplayName("다른 쓰레드가 이미 락을 점유하고 있으면 락 획득 실패 예외가 발생한다.")
     @Test
-    void throwExceptionWhenLockAcquisitionFails() {
+    void throwExceptionWhenLockAcquisitionFails() throws InterruptedException {
         // given
         String lockKey = "lock:inventory:" + productId;
         RLock rLock = redissonClient.getLock(lockKey);
@@ -210,6 +210,9 @@ class StockManagerServiceTest {
                             }
                         });
         lockPreemptor.start();
+
+        // 다른 쓰레드가 락을 획득할 수 있도록 잠시 대기
+        Thread.sleep(1000);
 
         // then
         assertThatThrownBy(
